3dcf2345351082c2fdb8ba038c48f8ac8baeadbe
Shachar Oz <shachar.oz@gmail.com>
2020-07-19 17:38:13 +0200

    converting phone camera image to a texture2d

---
+++ Assets/FileSaver.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class FileSaver : MonoBehaviour
+{
+
+    public UnityEngine.UI.Text logger;
+
+    void Start()
+    {
+    }
+
+    
+
+
+    public void SetTexture(Texture2D _texture)
+    {
+        lastSavedScreenshot = _texture;
+    }
+
+    private Texture2D lastSavedScreenshot;
+
+    public UnityEngine.Events.UnityEvent OnFileSavedSuccessfully;
+
+
+    private string GetFileName(string app_name = "")
+    {
+        string filename = app_name;
+        filename += DateTime.Now.ToString() + "_" + (UnityEngine.Random.value * 1000).ToString("0:000");
+
+        filename += ".png";
+        return filename;
+    }
+
+
+
+    
+    public void StartSaveProcessWithGrabber()
+    {
+        logger.text = "Permission 111: " + NativeGallery.RequestPermission();
+
+        StartCoroutine(TakeScreenshotAndSave());
+    }
+
+    private IEnumerator TakeScreenshotAndSave()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        // Save the screenshot to Gallery/Photos
+        logger.text = "Permission 222: " + NativeGallery.SaveImageToGallery(ss, "GalleryTest", "Image.png", NativeGalleryError);
+
+        // To avoid memory leaks
+        Destroy(ss);
+
+        OnFileSavedSuccessfully.Invoke();
+    }
+    private void NativeGalleryError(string result_error)
+    {
+        Debug.Log(result_error);
+        logger.text = result_error;
+    }
+
+
+}
--- Assets/ScreenshotMobileCatcher/ScreenshotMobileSaver.cs
+++ Assets/ScreenshotMobileCatcher/ScreenshotMobileSaver.cs
@@ -48,42 +48,55 @@ public class ScreenshotMobileSaver : MonoBehaviour
         {
             Debug.Log("Image path: " + path);
 
-
-
-
-            // Create Texture from selected image
-            Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
-            if (texture == null)
-            {
-                Debug.Log("Couldn't load texture from " + path);
-                return;
-            }
-
-            // Assign texture to a temporary quad and destroy it after 5 seconds
-            GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-            quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
-            quad.transform.forward = Camera.main.transform.forward;
-            quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
-
-            Material material = quad.GetComponent<Renderer>().material;
-            if (!material.shader.isSupported) // happens when Standard shader is not included in the build
-                material.shader = Shader.Find("Legacy Shaders/Diffuse");
-
-            material.mainTexture = texture;
-
-            Destroy(quad, 5f);
-
-            // If a procedural texture is not destroyed manually, 
-            // it will only be freed after a scene change
-            Destroy(texture, 5f);
-
-
         }, maxSize);
 
         Debug.Log("Permission result: " + permission);
     }
 
+    /*
+    private void TakePictureWithNativeCameraAndShowIt(int maxSize)
+    {
+        NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+        {
+            Debug.Log("Image path: " + path);
 
+
+            PresentPhotoFromPathOnPhone(path, maxSize);
+
+        }, maxSize);
+
+        Debug.Log("Permission result: " + permission);
+    }*/
+
+    private void PresentPhotoFromPathOnPhone(string _path, int _maxsize)
+    {
+        // Create Texture from selected image
+        Texture2D texture = NativeGallery.LoadImageAtPath(_path, _maxsize);
+        if (texture == null)
+        {
+            Debug.Log("Couldn't load texture from " + _path);
+            return;
+        }
+
+        // Assign texture to a temporary quad and destroy it after 5 seconds
+        GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
+        quad.transform.forward = Camera.main.transform.forward;
+        quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
+
+        Material material = quad.GetComponent<Renderer>().material;
+        if (!material.shader.isSupported) // happens when Standard shader is not included in the build
+            material.shader = Shader.Find("Legacy Shaders/Diffuse");
+
+        material.mainTexture = texture;
+
+        Destroy(quad, 5f);
+
+        // If a procedural texture is not destroyed manually, 
+        // it will only be freed after a scene change
+        Destroy(texture, 5f);
+
+    }
 
     private IEnumerator TakeScreenshotAndSave()
     {
@@ -118,31 +131,7 @@ public class ScreenshotMobileSaver : MonoBehaviour
             Debug.Log("Image path: " + path);
             if (path != null)
             {
-                // Create Texture from selected image
-                Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
-                if (texture == null)
-                {
-                    Debug.Log("Couldn't load texture from " + path);
-                    return;
-                }
-
-                // Assign texture to a temporary quad and destroy it after 5 seconds
-                GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                quad.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.5f;
-                quad.transform.forward = Camera.main.transform.forward;
-                quad.transform.localScale = new Vector3(1f, texture.height / (float)texture.width, 1f);
-
-                Material material = quad.GetComponent<Renderer>().material;
-                if (!material.shader.isSupported) // happens when Standard shader is not included in the build
-                    material.shader = Shader.Find("Legacy Shaders/Diffuse");
-
-                material.mainTexture = texture;
-
-                Destroy(quad, 5f);
-
-                // If a procedural texture is not destroyed manually, 
-                // it will only be freed after a scene change
-                Destroy(texture, 5f);
+                PresentPhotoFromPathOnPhone(path, maxSize);
             }
         }, "Select a PNG image", "image/png");
 
